3e4a0327a33bafb5fa157371f469d0a92f650773
github username <github email address>
2017-06-21 14:06:40 -0400

    Done.

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React from 'react'
 
 class Circle extends React.Component {
 
+  shouldComponentUpdate(nextProps) {
+    return Boolean(nextProps.color !== this.props.color)
+  }
+
   render() {
-    const { color } = this.props;
-    console.log(`A ${color} circle was rendered!`);
-    
+    const { color } = this.props
+
     return (
       <div className={`circle ${color}`}></div>
     )
   }
 }
 
-export default Circle;
+export default Circle
--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React from 'react'
 
 class Rating extends React.Component {
 
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       increasing: false,
       decreasing: false
-    };
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      increasing: this.props.rating < nextProps.rating,
+      decreasing: this.props.rating > nextProps.rating
+    })
   }
 
   render() {
-    let trend = 'stable';
+    let trend = 'stable'
     if (this.state.increasing) {
-      trend = 'increasing';
+      trend = 'increasing'
     } else if (this.state.decreasing) {
-      trend = 'decreasing';
+      trend = 'decreasing'
     }
 
     return (
@@ -28,4 +35,4 @@ class Rating extends React.Component {
   }
 }
 
-export default Rating;
+export default Rating
